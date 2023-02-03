ab8bd31ddced2fac1cb01269065a444c71c79e2f
Firdausi Maryamah <123624866+Auchley@users.noreply.github.com>
2023-02-03 23:04:16 +0700

    Add files via upload

---
+++ Auchley.py
@@ -0,0 +1,45 @@
+#name = input('what is your name?')
+#print('Hello '+name+' this is your first lessons in Python, so glad to see you :), hope you enjoy')
+
+#num1 = 101
+#num2 = 501
+#num3 = 203
+#num4 = 100
+#num5 = 1000
+#print('total number: '+ str(num1+num2+num3+num4+num5))
+
+#name = input('What is your name?')
+#age = int(input('how old are you?'))
+#yearsto100 = 100 - age
+#print('Hello '+name+', i think you will be 100 in '+str(yearsto100)+' years, so happy to know about you!! see you soon')
+
+#name = input('What is your name?')
+#age = int(input('how old are you?'))
+
+#yearsto50 = 50 - age
+#if yearsto50>0:
+    #print('Hello '+name+', i think you will be 50 in '+str(yearsto50)+' years, so happy to know about you!! see you soon')
+#else:
+    #print('hello'+name+', i think you were 50 '+str(-yearsto50)+' years ago, so happy to know about you!! see you soon')
+#print('Nice to meet you')
+
+#f = 100
+#while f<=1000:
+    #print(f)
+    #f = f + 1
+
+#exp = -1
+#total = 0
+#maxexp = 0
+#minexp =0
+#while exp!=0:
+    #exp = int(input ('what is the expense? (type 0 to stop)'))
+    #total = total + exp
+    #if exp>maxexp:
+        #maxexp =exp
+#print('Your total expenditure is ' + str (total))
+#print('The maximum you spent is ' +str(maxexp))
+
+exp = (12, 18, 10, 19, 23, 10)
+print (exp)
+print(len(exp))
